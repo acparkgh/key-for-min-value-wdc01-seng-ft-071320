220af4fc4d903c5e4b8e36122ac8ea729bad2871
Andrew Park <acpark123@gmail.com>
2020-06-11 02:14:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+
+   
+  name_array = name_hash.to_a
+  name_array.empty? ? smallest_key = nil : smallest_key = name_array[0][0]
+  smallest_value = name_array[0][1].to_i
+  name_hash.each do | key, value |
+
+    if name_hash[key] < smallest_value
+      smallest_value = name_hash[key]
+      smallest_key = key
+    end
+  end
+
+  smallest_key
+
 end
